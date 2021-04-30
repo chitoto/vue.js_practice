dba506e4bcfbdb60619a206178a411989c5b8147
Yumiko Kamiyama <tototina.ruru@gmail.com>
2021-04-30 19:42:07 +0900

    practice

--- js/main.js
+++ js/main.js
@@ -1,10 +1,15 @@
-Vue.component('hello-component', {
-  template: '<p>Hello</p>'
-})
-
 var app = new Vue({
   el: '#app',
   data: {
-    message: "Hello vue.js!"
+    message: 'Hello World!',
+    url: 'https://jp.vuejs.org/',
+    toggle: true,
+    languages: ['javascript', 'Ruby', 'Python']
+  },
+  methods: {
+    onclick: function() {
+      this.message = "clicked!"
+    }
   }
+
 })
