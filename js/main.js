98eed31e8cebb5ce1c2e4629a4ef0fd40416dbfb
Yumiko Kamiyama <tototina.ruru@gmail.com>
2021-04-30 14:46:00 +0900

    object_Hairetsu

--- js/main.js
+++ js/main.js
@@ -1,6 +1,13 @@
 new Vue({
   el: '#app',
   data: {
-    message: 'Hello Vue.js!'
+    message: 'Hello Vue.js!',
+    count: 0,
+    user: {
+      lastName: 'Yamada',
+      firstName: 'Taro',
+      prefecture: 'Tokyo',
+    },
+    colors: ['Red', 'Green', 'Blue']
   }
 })
