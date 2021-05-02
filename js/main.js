b159d7659875cb9e370fd2c10a43aff4e0a3e91a
Yumiko Kamiyama <tototina.ruru@gmail.com>
2021-05-02 17:42:15 +0900

    add:v-pre

--- js/main.js
+++ js/main.js
@@ -2,11 +2,6 @@ var app = new Vue({
   el: '#app',
   data: {
     message: 'Hello! Vue.js!'
-  },
-  methods: {
-    clickHandler: function(event) {
-      this.message = this.message.split('').reverse().join('')
-    }
   }
 
 })
