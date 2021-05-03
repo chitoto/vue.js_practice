e4edef545424d448b00e406e5cb77555af485015
Yumiko Kamiyama <tototina.ruru@gmail.com>
2021-05-03 17:55:47 +0900

    Calculated property

--- js/main.js
+++ js/main.js
@@ -1,11 +1,11 @@
-// suffix とは 接尾辞(...とか)
-Vue.filter('readMore', function(text, length, suffix){
-  return text.substring(0, length) + suffix
-})
-
 var app = new Vue({
   el: '#app',
   data: {
-    url: 'https://google.co.jp'
+    message: 'Hello Vue.js!'
+  },
+  computed: {
+    reversedMessage: function() {
+      return this.message.split('').reverse().join('')
+    }
   }
 })
