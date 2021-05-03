9d8a68b031b735fd8524c2f0e359ba658ea91888
Yumiko Kamiyama <tototina.ruru@gmail.com>
2021-05-03 17:02:17 +0900

    add:global_filter

--- js/main.js
+++ js/main.js
@@ -1,12 +1,17 @@
+// グローバルフィルタ
+Vue.filter('numberFormat', function(value) {
+  return value.toLocaleString()
+})
+
 var app = new Vue({
   el: '#app',
   data: {
     price: 29800
   },
-  filters: {
-    numberFormat: function(value) {
-      return value.toLocaleString()
-    }
-  }
+  // filters: {
+  //   numberFormat: function(value) {
+  //     return value.toLocaleString()
+  //   }
+  // }
 
 })
